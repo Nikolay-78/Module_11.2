a287c7e424aa2b9801a19762a3cf1945bb9c069c
Николай <kalyam@mail.ru>
2025-04-29 10:02:32 +0300

    Добавьте файлы проекта.

---
+++ Module_11.2_Var_Const_Liter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Module_11._2_Var_Const_Liter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string Myname = "Василиса";
+            const string Myname2 = "Мартин";         
+            Console.WriteLine("\t Меня зовут {0}. \n \t А мою собаку {1}.", Myname, Myname2);
+            Console.WriteLine("\t Я играю со своей собакой. \n \t Мартин громко лает и резвится.");
+            Console.ReadKey();
+        }
+    }
+}
